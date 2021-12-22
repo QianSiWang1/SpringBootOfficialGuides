32d7da871d0c9a1804c830193708a53f937de15b
钱誉 <597667983@qq.com>
2021-12-22 12:02:37 +0800

    first commit

---
+++ GettingStartedGuides/BuildingaRestfulWebService/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+/**
+ * @author YuQian
+ * @create 2021/11/24
+ * @Description
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
